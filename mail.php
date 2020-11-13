32fba5e9a2862529072d500f9cecd1898e616af0
densidnay@gmail.com <Jesuschrist05882>
2020-11-13 13:22:21 +0300

    change mail php

--- mail.php
+++ mail.php
@@ -12,30 +12,6 @@ $mailSpam = htmlspecialchars(trim($_POST["mail"]));
 $title = htmlspecialchars($_POST["title"]);
 $phone = htmlspecialchars($_POST["phone"]);
 $agree = htmlspecialchars($_POST["agree"]);
-// if( isset($_POST["g-recaptcha-response"]) && strlen(trim($_POST["g-recaptcha-response"]))> 0 ) {
-    
-//     $captcha_Secret = '6LcR-9IZAAAAABqAF07RZM11zPH69gNGg7ynGThE';
-//     $recaptcha_Response = trim($_POST["g-recaptcha-response"]);
-
-//     $ch = curl_init();
-// 	$google_fields = array(
-// 		'secret'   => $captcha_Secret,
-// 		'response' => $recaptcha_Response,
-// 		'remoteip' => $_SERVER["REMOTE_ADDR"]
-// 	);
-    
-// 	//set the url, number of POST vars, POST data
-// 	curl_setopt($ch, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
-// 	curl_setopt($ch, CURLOPT_POST, true);
-// 	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($google_fields));
-// 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-// 	//execute post
-// 	$post_result = curl_exec($ch);
-// 	$google_response = json_decode($post_result, true);
-// 	curl_close($ch);
-
-//     if ( $google_response["success"] == 1 ) {        
     
         if ( isset($_POST["mail"] )  && strlen(trim($_POST["mail"])) == 0 ) {
             // $to = "traffic@alliance-trucks.ru";
